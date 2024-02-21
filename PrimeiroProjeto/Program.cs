a50cf2e0353e5963d00a21b518010ce75d89ed97
CJ3022765 <CJ3022765@LAB01-PC18>
2024-02-21 10:29:03 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Threading.Channels;
+
+string nome = "Renato";
+int idade = 15;
+string city = "Campos Do Jordão";
+
+Console.WriteLine("Escreva seu nome");
+nome = Console.ReadLine();
+Console.WriteLine("escreva sua idade");
+idade = int.Parse(Console.ReadLine());
+Console.WriteLine("digite a sua cidade");
+city= Console.ReadLine();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World");
+Console.WriteLine("Nome: {0}", nome);
+Console.WriteLine("idade: {0}", idade);
+Console.WriteLine("cidade: {0}", city);
+//Console.WriteLine("Nome: {0} \nIdade: {1} \nCidade: {2}", nome, idade, city );
+
+
+
