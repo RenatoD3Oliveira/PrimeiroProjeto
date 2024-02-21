d6daab682b0e02c4acafd4628de713574bdb14e9
Renato <renato.moreira@aluno.ifsp.edu.br>
2024-02-21 11:10:14 -0300

    construindo calculadora

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,23 +1,32 @@
 ﻿
 using System.Threading.Channels;
 
-string nome = "Renato";
-int idade = 15;
-string city = "Campos Do Jordão";
+//string nome = "Renato";
+//int idade = 15;
+//string city = "Campos Do Jordão";
 
-Console.WriteLine("Escreva seu nome");
-nome = Console.ReadLine();
-Console.WriteLine("escreva sua idade");
-idade = int.Parse(Console.ReadLine());
-Console.WriteLine("digite a sua cidade");
-city= Console.ReadLine();
+//Console.WriteLine("Escreva seu nome");
+//nome = Console.ReadLine();
+//Console.WriteLine("escreva sua idade");
+//idade = int.Parse(Console.ReadLine());
+//Console.WriteLine("digite a sua cidade");
+//city= Console.ReadLine();
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World");
-Console.WriteLine("Nome: {0}", nome);
-Console.WriteLine("idade: {0}", idade);
-Console.WriteLine("cidade: {0}", city);
-//Console.WriteLine("Nome: {0} \nIdade: {1} \nCidade: {2}", nome, idade, city );
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World");
+//Console.WriteLine("Nome: {0}", nome);
+//Console.WriteLine("idade: {0}", idade);
+//Console.WriteLine("cidade: {0}", city);
+////Console.WriteLine("Nome: {0} \nIdade: {1} \nCidade: {2}", nome, idade, city );
+// ctrl+k+u
+
+Console.WriteLine("digite um numero");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("digite outro numero");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("escolha a operação,entre adição,subtraçao,divisão e multiplicação");
+string operação = Console.ReadLine();
 
 
 
